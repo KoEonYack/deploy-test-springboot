8cce9169b0ab2145394bc3ae4434c0a23f70e088
koeonyack <covenant.ko@kakao.com>
2021-02-10 10:42:33 +0900

    init

---
+++ src/main/java/com/example/webdemo/WebController.java
@@ -0,0 +1,28 @@
+package com.example.webdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//public class WebController {
+//
+//    @Autowired
+//    private WebService webService;
+//
+//    @GetMapping("")
+//    public String homepage() {
+//        return webService.covenant();
+//    }
+//}
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    @ResponseBody
+    public String main() {
+        return "<a href=\"hhttp://covenant.tistory.com/\">covenant.tistory.com/</a>";
+    }
+}
---
+++ src/main/java/com/example/webdemo/WebService.java
@@ -0,0 +1,12 @@
+package com.example.webdemo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebService {
+
+    public String covenant() {
+        return "<h1>http://covenant.tistory.com/</h1>";
+    }
+
+}
