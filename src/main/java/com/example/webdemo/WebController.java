80b5945427fc093e9d3931dbd1c45a7dae3e8432
koeonyack <covenant.ko@kakao.com>
2021-02-10 15:03:28 +0900

    add 11st link

--- src/main/java/com/example/webdemo/WebController.java
+++ src/main/java/com/example/webdemo/WebController.java
@@ -1,28 +1,16 @@
 package com.example.webdemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-//@RestController
-//public class WebController {
-//
-//    @Autowired
-//    private WebService webService;
-//
-//    @GetMapping("")
-//    public String homepage() {
-//        return webService.covenant();
-//    }
-//}
 
 @Controller
 public class WebController {
     @GetMapping("/")
     @ResponseBody
     public String main() {
-        return "<a href=\"https://covenant.tistory.com/\">covenant.tistory.com/</a>";
+        String html = "<a href=\"https://covenant.tistory.com/\">covenant.tistory.com/</a><br />";
+        html += "<a href=\"https://11st.co.kr/\">11st.co.kr</a>";
+        return html;
     }
 }
